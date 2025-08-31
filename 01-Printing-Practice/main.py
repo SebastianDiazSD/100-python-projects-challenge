7e4a09fe6bad6b2fc0730e5097aa7ec937734955
sdiaz1934 <sebas.a.diaz91@gmail.com>
2025-08-31 18:33:15 +0200

    Create main.py

---
+++ 01-Printing-Practice/main.py
@@ -0,0 +1,11 @@
+# 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+
+print(f"1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n"
+      f"2. Knead the dough for 10 minutes.\n"
+      f"3. Add 3g of Salt.\n"
+      f"4. Leave to rise for 2 hours.\n"
+      f"5. Bake at 200 degrees C for 30 minutes.")
