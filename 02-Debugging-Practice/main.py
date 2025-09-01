718ba756b57c566b121fc1c8767fc1a0e545addb
SebastianDiazSD <124620220+SebastianDiazSD@users.noreply.github.com>
2025-09-01 18:34:14 +0200

    Add files via upload

---
+++ 02-Debugging-Practice/main.py
@@ -0,0 +1,5 @@
+print("Notes from Day 1") #SyntaxError: unterminated string literal (detected at line 1)
+print("The print statement is used to output strings") #IndentationError: unexpected indent
+print("Strings are strings of characters") #SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print("String Concatenation is done with the + sign") #NameError: name 'priint' is not defined. Did you mean: 'print'?
+print("New lines can be created with a \ and the letter n") #SyntaxError: '(' was never closed
