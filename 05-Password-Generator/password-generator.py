a7eea882bf81815ecbd64fd18757488f4cc6766a
sdiaz1934 <sebas.a.diaz91@gmail.com>
2025-09-05 17:27:26 +0200

    Create password-generator.py

---
+++ 05-Password-Generator/password-generator.py
@@ -0,0 +1,126 @@
+logo = """
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄                  
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌                 
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌                
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌                
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌   ▄   ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌                
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌                
+▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌▐░▌ ▐░▌░▌ ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀ ▐░▌       ▐░▌                
+▐░▌          ▐░▌       ▐░▌          ▐░▌          ▐░▌▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌▐░▌     ▐░▌  ▐░▌       ▐░▌                
+▐░▌          ▐░▌       ▐░▌ ▄▄▄▄▄▄▄▄▄█░▌ ▄▄▄▄▄▄▄▄▄█░▌▐░▌░▌   ▐░▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄█░▌                
+▐░▌          ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░▌                 
+ ▀            ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀       ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀                  
+                                                                                                                        
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌
+▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌
+▐░▌          ▐░▌          ▐░▌▐░▌    ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌
+▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌
+▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌
+▐░▌ ▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀█░▌     ▐░▌     ▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀ ▐░▌
+▐░▌       ▐░▌▐░▌          ▐░▌    ▐░▌▐░▌▐░▌          ▐░▌     ▐░▌  ▐░▌       ▐░▌     ▐░▌     ▐░▌       ▐░▌▐░▌     ▐░▌   ▀ 
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ ▐░▌       ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌  ▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌
+ ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀ 
+"""
+
+print(logo)
+
+# Welcoming message
+print("Welcome to the PyPassword Generator!\nWe are making your life safer, parcero!\nLet's get started!!")
+print("\nHint: Your password must be at least 8 characters long")
+
+# Setting the code input parameters
+# Constants
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# Variables or user input
+char_letters = False
+number_letters = input("\nHow many letters would you like in your password, maniño?\n")
+while not char_letters:
+    if number_letters.isnumeric():
+        number_letters = int(number_letters)
+        char_letters = True
+    else:
+        number_letters = input("\nPapi! That is not valid. Try again\n"
+                               "How many letters would you like in your password?\n")
+
+char_numbers = False
+number_numbers = input("\nHow many numbers would you like in your password, socio?\n")
+while not char_numbers:
+    if number_numbers.isnumeric():
+        number_numbers = int(number_numbers)
+        char_numbers = True
+    else:
+        number_numbers = input("\nPanaa! That is not valid. Try again\n"
+                               "How many numbers would you like in your password?\n")
+
+char_symbols = False
+number_symbols = input("\nHow many symbols would you like in your password, llave?\n")
+while not char_symbols:
+    if number_symbols.isnumeric():
+        number_symbols = int(number_symbols)
+        char_symbols = True
+    else:
+        number_symbols = input("\nSocio ome! That is not valid. Try again\n"
+                               "How many numbers would you like in your password?\n")
+
+# Variable to store each character of the password
+password_list = []
+
+# Password generated
+structured_password = ""  # Characters are grouped by type, but shuffled within each group
+scrambled_password = ""  # All characters (letters, numbers, symbols) are mixed completely at random
+
+# Does the password meet the length requirement?
+length = False
+password_length = number_letters + number_numbers + number_symbols
+
+while not length:
+    if password_length >= 8:
+        length = True
+    else:
+        print("\nOjo con eso! Your password is not long enough! Try again!")
+        number_letters = int(input("\nHow many letters would you like in your password, maniño?\n"))
+        number_numbers = int(input("\nHow many numbers would you like in your password, socio?\n"))
+        number_symbols = int(input("\nHow many symbols would you like in your password, llave?\n"))
+        password_length = number_letters + number_numbers + number_symbols
+
+import random
+
+# Adding the corresponding quantity of letters to the password list
+for char in range(0, number_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, number_numbers):
+    password_list.append(random.choice(numbers))
+
+for char in range(0, number_symbols):
+    password_list.append(random.choice(symbols))
+
+# Structured Mode or Scrambled Mode
+structured_scrambled = input("\nDo you want to have a structured password[1] or a scrambled one[2]?\n")
+checker = False
+while not checker:
+    if structured_scrambled == "1" or structured_scrambled == "2":
+        structured_scrambled = int(structured_scrambled)
+        checker = True
+    else:
+        structured_scrambled = input("\nParcerito! That is not valid. Try again\n"
+                                     "structured password[1] or a scrambled one[2]?\n")
+
+if structured_scrambled == 1:
+    for char in password_list:
+        structured_password += char
+    print(f"\nHere is your password panita!\n{structured_password}")
+else:
+    random.shuffle(password_list)
+    for char in password_list:
+        scrambled_password += char
+    print(f"\nHere is your password panita!\n{scrambled_password}")
