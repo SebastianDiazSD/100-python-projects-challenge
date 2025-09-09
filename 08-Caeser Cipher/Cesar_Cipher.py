1474158a800acbaeeabbe677d1a8e94621ed739c
sdiaz1934 <sebas.a.diaz91@gmail.com>
2025-09-09 17:04:29 +0200

    Create Cesar_Cipher.py

---
+++ 08-Caeser Cipher/Cesar_Cipher.py
@@ -0,0 +1,98 @@
+def cipher():
+
+    print("""           
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣿⣿⢿⡶⠆⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⡿⠻⠋⣠⠀⢀⣶⠇⢠⣾⡿⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣼⠟⠋⠻⢁⣴⠀⣾⣿⠀⠾⠟⠀⠈⣉⣠⣦⡤⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠸⠃⣠⡆⠀⣿⡟⠀⠛⠃⠀⠀⣶⣶⣦⣄⠉⢁⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⡀⢰⣿⠇⠀⢉⣀⣀⠛⠿⠿⠦⠀⢀⣠⣤⣴⣾⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⠃⠀⠠⣴⣦⡈⠙⠛⠓⠀⢰⣶⣶⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀
+⠀⠀⢀⣤⠦⡀⠰⢷⣦⠈⠉⠉⠀⣰⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀
+⠀⠀⠈⠁⠀⠘⣶⣤⣄⣀⣨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣯⡈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢨⣿⣿⣿⣷⣤⣈⡉⠛⠛⠛⠛⠻⠟⠛⠛⠛⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀           
+Bienvenid@s! 
+This is the Caesar Cipher Program! Keep your messages secret!
+Nadie se va enterar!
+
+""")
+    
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    while direction not in ("encode","decode"):
+            print("That is not a valid option")
+            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift=shift%26 #in case the user inputs a number greater than 26 - Lenght of the alphabet -
+
+    #Shifting alphabet
+
+    shift2=shift
+
+    #Encrypting/Decrypting
+
+
+    def encrypt_decrypt():
+        position_encrypt=0
+        position_decrypt=0
+        encrypt_text=''
+        decrypt_text=''
+        if direction == "encode":
+            for letter in text:
+                if letter in alphabet:
+                    position_encrypt=alphabet.index(letter)
+                    new_position=position_encrypt+shift2
+                    encrypt_text+=str(alphabet[new_position])
+                else:
+                    encrypt_text+=str(letter)
+
+            print(f"The encoded text is '{encrypt_text}'")
+        elif direction == "decode":
+            for letter in text:
+                if letter in alphabet:
+                    position_decrypt=alphabet.index(letter)
+                    new_position=position_decrypt-shift2
+                    decrypt_text+=str(alphabet[new_position])
+                else:
+                    decrypt_text+=str(letter)
+
+            print(f"The decoded text is '{decrypt_text}'")
+
+    encrypt_decrypt()
+
+    #Re running
+   
+    user_choice = input("Do you want to run the Cipher again? Type 'Yes' or 'No':\n").lower()
+    while user_choice not in ("yes","no"):
+            print("That is not a valid option")
+            user_choice = input("Viejito! Do you want to run the cipher again? Type 'Yes' or 'No':\n").lower()
+    should_continue=True
+    while should_continue is True:
+        if user_choice =="yes":
+            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+            while direction not in ("encode","decode"):
+                print("That is not a valid option")
+                direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+            text = input("Type your message:\n").lower()
+            shift = int(input("Type the shift number:\n"))
+            shift=shift%26 #in case the user inputs a number greater than 26 - Lenght of the alphabet -
+
+            encrypt_decrypt()
+            user_choice = input("Viejto! Do you want to run the Cipher again? Type 'Yes' or 'No':\n").lower()
+            while user_choice not in ("yes","no"):
+                print("Eee Pana! That is not a valid option")
+                user_choice = input("Viejito! Do you want to run the cipher again? Type 'Yes' or 'No':\n").lower()
+        else:
+            print("Pana! Thank you very much for using our Cipher program")
+            should_continue=False
+        
+cipher()
